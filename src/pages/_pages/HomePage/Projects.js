866c5371fe723185314cb86b217f9186f305dd84
dev0715 <toptal126@outlook.com>
2023-04-27 04:16:25 +0800

    complete cads

--- src/pages/_pages/HomePage/Projects.js
+++ src/pages/_pages/HomePage/Projects.js
@@ -1,27 +1,7 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import { Fantasy, Solchicks, Bulkperks, Sidus, NetVRK } from 'utils/_utils/EntityFieldDefs';
-import Card from 'components/_components/Card';
+import React from 'react'
 
-const Projects = () => {
-  const projectArr = [Fantasy, Solchicks, Bulkperks, Sidus, NetVRK];
-
+export default function Projects() {
   return (
-    <Box
-      sx={{
-        '@media (max-width: 600px)': {
-          margin: '20px 2%'
-        },
-        margin: '20px 8%',
-        display: '-webkit-box',
-        overflowX: 'hidden !important'
-      }}
-    >
-      {projectArr.map((project) => (
-        <Card img={project.img} label={project.label} sx={{ marginRight: '40px', }} />
-      ))}
-    </Box>
-  );
-};
-
-export default Projects;
+    <div>Projects</div>
+  )
+}
--- src/pages/_pages/HomePage/index.js
+++ src/pages/_pages/HomePage/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Background from './Background';
+import Cards from './Cards';
 import Projects from './Projects';
 
 const HomePage = () => {
   return (
     <div>
       <Background />
+      <Cards />
       <Projects />
     </div>
   );
