5058792b574b6ce10dd9f25c386ef7f1adffbbe6
wangmufin <w.richard@mufin.co.kr>
2023-04-27 21:05:02 +0800

    complete main page

---
+++ src/components/_components/Input.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
+
+export const SearchInput = (props) => {
+  return (
+    <TextField
+      placeholder={props.placeholder}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        ),
+        style: { fontWeight: 100, backgroundColor: '#171717', maxWidth: '400px', height: '70px', fontSize: '20px' }
+      }}
+    />
+  );
+};
--- src/layouts/_layouts/main/Footer.js
+++ src/layouts/_layouts/main/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import { LH5, TLabel, GLabel } from 'components/_components/Label';
+import { LH5, TLabel, GLabel, LH4 } from 'components/_components/Label';
 import { IconButtonGroup } from 'components/_components/Button';
 import {
   loremString,
@@ -13,48 +13,78 @@ import {
 } from 'utils/_utils/EntityFieldDefs';
 const Footer = () => {
   return (
-    <Box
-      sx={{
-        backgroundColor: '#171717',
-        padding: '70px 10%',
-        display: 'flex',
-        justifyContent: 'space-between',
-        borderBottom: '1px solid #002B15',
-        flexDirection: 'row',
-        '@media (max-width: 1360px)': {
-          flexDirection: 'column',
-          alignItems: 'center'
-        }
-      }}
-    >
-      <Box>
-        <Box height={64} sx={{ marginBottom: '60px' }}>
-          <img src="_img/footer.png" alt="Footer" height="100%" />
+    <Box>
+      <Box
+        sx={{
+          backgroundColor: '#171717',
+          padding: '70px 10%',
+          display: 'flex',
+          justifyContent: 'space-between',
+          borderBottom: '1px solid #002B15',
+          flexDirection: 'row',
+          '@media (max-width: 1360px)': {
+            flexDirection: 'column',
+            alignItems: 'center'
+          }
+        }}
+      >
+        <Box>
+          <Box height={64} sx={{ marginBottom: '60px' }}>
+            <img src="_img/footer.png" alt="Footer" height="100%" />
+          </Box>
+          <LH5
+            text={loremString}
+            sx={{
+              fontWeight: 100,
+              width: '612px',
+              '@media (max-width: 1360px)': {
+                width: '400px',
+                marginBottom: '50px'
+              },
+              '@media (max-width: 500px)': {
+                width: '100%',
+                marginBottom: '50px'
+              }
+            }}
+          />
         </Box>
-        <LH5
-          text={loremString}
-          sx={{
-            fontWeight: 100,
-            width: '612px',
-            '@media (max-width: 1360px)': {
-              width: '400px',
-              marginBottom: '50px'
-            }
-          }}
-        />
-      </Box>
 
-      <Box>
-        <Box display="flex" flexDirection="row" width={400} justifyContent="space-between">
-          <TLabel text="Join Our " />
-          <GLabel text="Community" />
+        <Box>
+          <Box
+            display="flex"
+            sx={{
+              '@media (max-width: 500px)': {
+                flexDirection: 'column'
+              }
+            }}
+            flexDirection="row"
+            maxWidth={400}
+            width="100%"
+            justifyContent="space-between"
+          >
+            <TLabel text="Join Our " sx={{ marginRight: '8px' }} />
+            <GLabel text="Community" />
+          </Box>
+          <IconButtonGroup
+            sx={{ marginTop: '30px' }}
+            size="50px"
+            elements={[twitter, facebook, instagram, reddit, linkedin]}
+          />
+          <LH5 text={privacyString} sx={{ fontWeight: 100, marginTop: '40px' }} />
         </Box>
-        <IconButtonGroup
-          sx={{ marginTop: '30px' }}
-          size="50px"
-          elements={[twitter, facebook, instagram, reddit, linkedin]}
+      </Box>
+      <Box
+        sx={{
+          width: '100%',
+          backgroundColor: '#171717',
+          display: 'flex',
+          justifyContent: 'center'
+        }}
+      >
+        <LH4
+          text="© Copyrighted Shardstarter Network 2023 | All Rights Reserved"
+          sx={{ padding: '20px 0px', textAlign: 'center' }}
         />
-        <LH5 text={privacyString} sx={{ fontWeight: 100, marginTop: '40px' }} />
       </Box>
     </Box>
   );
