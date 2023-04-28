d9e9673abe221b393bc78c7ede4e789336c91e9d
ericpalmer324 <ericpalmer.software@gmail.com>
2023-04-28 16:13:43 -0400

    Change Staking

--- src/pages/_pages/StakingPage/index.js
+++ src/pages/_pages/StakingPage/index.js
@@ -1,31 +1,13 @@
 import React from 'react';
-import Box from '@mui/material/Box';
-import LeftStaking from './LeftStaking';
-import RightStaking from './RightStaking';
+import MainStaking from "./MainStaking"
+import PriceStaking from "./PriceStaking"
 
 const StakingPage = () => {
   return(
-    <Box
-      sx={{
-        '@media (max-width: 1500px)': {
-          width: '100%',
-          marginLeft: '0px'
-        },
-        backgroundImage: 'url("_img/staking/background.png")',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        borderRadius: '38px',
-        padding: '60px 65px',
-        marginLeft: '150px',
-        marginRight: '150px',
-        display: 'flex',
-        justifyContent:'space-between'
-      }}
-    >
-      <LeftStaking />
-      <RightStaking />
-    </Box>
+    <div>
+      <MainStaking />
+      <PriceStaking />
+    </div>
   )
 }
 
